b9104b09bee939844b891edfedf9aac29588f4db
M7MD_Reda <mohamedredaelsawy0@gmail.com>
2026-04-24 02:44:07 +0300

    Add files via upload

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("mohamed", 20, 85.5);
+            student1.displayInfo();
+            student1.setAge(4); 
+            student1.setGrade(105);
+            student1.setAge(22); 
+            student1.setGrade(90); 
+            student1.displayInfo();
+        }
+    }
+}
---
+++ Day4/Day4/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class Student
+    {
+        private string name ;
+        private int age;
+        private double grade;
+
+        public Student(string name, int age, double grade)
+        {
+            this.name = name;
+            this.age = age;
+            this.grade = grade;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public int getAge()
+        {
+            return age;
+        }
+        public void setAge(int age)
+        {
+            if (age > 5)
+            {
+                this.age = age;
+            }
+            else
+            {
+                Console.WriteLine("Age must be greater than 5");
+            }
+        }
+        public double getGrade()
+        {
+            return grade;
+        }
+        public void setGrade(double grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grade = grade;
+            }
+            else
+            {
+                Console.WriteLine("Grade must be between 0 and 100");
+            }
+        }
+        public void displayInfo()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Grade: " + grade);
+        }
+    }
+}
